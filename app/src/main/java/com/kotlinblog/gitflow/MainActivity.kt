881dd326e44e9c83d34b2fdba81ee1518c138d8c
Jan Slominski <jan.slominski@dyson.com>
2018-04-24 09:41:20 +0100

    abc-222 adding log tag

--- app/src/main/java/com/kotlinblog/gitflow/MainActivity.kt
+++ app/src/main/java/com/kotlinblog/gitflow/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kotlinblog.gitflow
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Adding new feature!!!
+        Log.d("TAG1", "test")
     }
 }
